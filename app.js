d86302ded29f32d577ed73a3fddb5dd971b7368b
Telugu Renuka <telugurenuka40@gmail.com>
2025-05-26 01:13:40 +0530

    Reviews feature added

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
-const { listingSchema } = require('./schema.js');
+const { listingSchema, reviewSchema } = require('./schema.js');
+const Review = require('./models/review.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -42,6 +43,15 @@ const validateListing = (req, res, next) => {
     next();
 };
 
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        return next(new ExpressError(400, errMsg));
+    }
+    next();
+};
+
 // Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
     let allListings = await Listing.find({});
@@ -56,7 +66,7 @@ app.get("/listings/new", (req, res) => {
 
 // Show Route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
+    let listing = await Listing.findById(req.params.id).populate("reviews");
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -86,6 +96,28 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+// Reviews
+// Post Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+
+    console.log("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+// Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
 }));
 
 // app.get("/testListing", async (req, res) => {
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review.js');
 
 const listingSchema = new Schema({
     title: {
@@ -29,6 +30,22 @@ const listingSchema = new Schema({
     country: {
         type: String,
         required: true
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing) {
+        await Review.deleteMany({
+            _id: {
+                $in: listing.reviews
+            }
+        });
     }
 });
 
